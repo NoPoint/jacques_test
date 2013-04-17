fcfe8ce4454cd15ee0b15640dcb704f9b02a536e
Jacques La Grange <normaldayhell@gmail.com>
2013-04-17 23:16:39 +0200

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JacquesTest::Application.config.secret_token = '73e6d0365d552bd12f8bd8000b49a58f20bf8263523122b808dbc947c475bd192316e8eeb3048f6bfbe195059e2509fe77c1ed0c7de8eb68374a89ad8b115de6'
